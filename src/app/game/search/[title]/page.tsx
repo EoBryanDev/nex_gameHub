976b58c8b05ba6cef670b3c19d4b418be508a8f5
EoBryanDev <mauriciobryan1998@gmail.com>
2023-11-25 18:30:53 -0300

    Finishing Search Page

---
+++ src/app/game/search/[title]/page.tsx
@@ -0,0 +1,46 @@
+import Card from "@/components/Card";
+import Container from "@/components/Container";
+import Input from "@/components/Input";
+import { IGameProps } from "@/utils/interface/IGameProps";
+
+interface ISearch {
+  params: {
+    title: string;
+  };
+}
+
+const getSpecifiedGame = async (title: string) => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_API_URL}/next-api/?api=game&title=${title}`,
+      { next: { revalidate: 320 } }
+    );
+    return res.json();
+  } catch (error) {
+    throw new Error("Failed to fech data!");
+  }
+};
+const Search: React.FC<ISearch> = async ({ params }: ISearch) => {
+  const game: IGameProps[] = await getSpecifiedGame(params.title);
+  return (
+    <main className='w-full text-black'>
+      <Container>
+        <Input type='text' placeholder='Looking for any game ? ' />
+
+        <h1 className='font-bold text-xl my-5'>
+          Look what we found for you...
+        </h1>
+
+        {!game && <p>There was not found any game with this title!</p>}
+
+        <section className='grid gap-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+          {game?.map((item) => (
+            <Card key={item.id} data={item} />
+          ))}
+        </section>
+      </Container>
+    </main>
+  );
+};
+
+export default Search;
